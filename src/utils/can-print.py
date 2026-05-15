362f8448eaeb59a68311d04c69f5cfd95e09f0ac
hwthomas <hywel.w.thomas@gmail.com>
2026-05-15 15:50:25 +0100

    Update can-print.py

--- src/utils/can-print.py
+++ src/utils/can-print.py
@@ -16,11 +16,12 @@ can_id = ""
 
 print('\n\rCAN Rx...Tx test')
 print('Bring up CAN0....')
+os.system("sudo /sbin/ip link set can0 down")    # Prevent 'Busy' error if already UP
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
 time.sleep(0.1)	
 
 try:
-	bus = can.interface.Bus(channel='can0', bustype='socketcan')
+	bus = can.Bus(channel='can0', interface='socketcan', can_filters=None)    # allow all CAN-IDs
 except OSError:
 	print('Cannot find CAN board.')
 	exit()
@@ -29,25 +30,17 @@ print('Ready')
 
 try:
 	while True:
-		message = bus.recv()	# Wait until a message is received.
+		message = bus.recv()	# Blocking wait until a message is received.
 		
 		c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
-		
-		can_id = message.arbitration_id
-		count = message.dlc
-
 		s=' ... '
 		for i in range(message.dlc ):
 			s +=  '{0:x} '.format(message.data[i])
-			
-		print(' {}'.format(c+s))
+			print(' {}'.format(c+s))
 		
-		# pause 10mS, then loop for next message or until Ctrl-C
-		time.sleep(0.010) 
-		count = 8
-		msg = can.Message(can_id,data=[0x00,0x01,0x02, 0x03, 0x04, 0x05,0x06, count & 0xff])
-		# bus.send(msg)
-	
+		# pause 100mS, then loop for next message or until Ctrl-C
+		time.sleep(0.100) 
+		
 except KeyboardInterrupt:
 	# Catch keyboard interrupt
 	print('\n\rKeyboard interrupt')	
