a213c50797921b57e4f6c7bb11745ec623b5cc9f
hwthomas <hywel.w.thomas@gmail.com>
2026-08-07 14:43:00 +0100

    try decode methods interactively

--- src/utils/can_decode.py
+++ src/utils/can_decode.py
@@ -40,4 +40,32 @@ with open('file.csv') as csvDataFile:
 print(dates)
 print(scores)
 
-#
+# simple readline from file 'short.log'
+
+Time Stamp,ID,Extended,Dir,Bus,LEN,D1,D2,D3,D4,D5,D6,D7,D8
+-1688467643383897,00000201,false,Rx,0,8,02,00,00,00,00,00,00,00,
+-1688467643373880,00000700,false,Rx,0,8,01,02,00,00,06,00,00,00,
+-1688467643363869,00000202,false,Rx,0,8,01,01,01,00,00,00,00,00,
+-1688467643355760,00000108,false,Rx,0,8,00,F4,01,87,B3,01,00,00,
+
+
+file_path = 'input.txt'
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+
+#or read 1 line    
+with open("short.log") as file:
+    print(file.readline())    
+
+# This loop prints 1 line at a time    
+with open("short.log") as file:
+    for line in file:
+        print(line)    
+
+>>> with open(file_path,'r') as file:
+...     for line in file:
+...         print(line)
+...         ts = line[0:17]
+...         len = line[38]
+...         id = line[23:26]
+...         print("ts = ", ts, "len = ", len, "id = ", id)
