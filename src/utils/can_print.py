db1d7ca8d8cfdbbcbf02d64bb2abac5a58cf62a6
hwthomas <hywel.w.thomas@gmail.com>
2026-08-23 16:55:48 +0100

    update can_print.py

--- src/utils/can_print.py
+++ src/utils/can_print.py
@@ -18,34 +18,34 @@ print('\n\rCAN Rx...Tx test')
 print('Bring up CAN0....')
 os.system("sudo /sbin/ip link set can0 down")    # Prevent 'Busy' error if already UP
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
-time.sleep(0.1)	
+time.sleep(0.1) 
 
 try:
-	bus = can.Bus(channel='can0', interface='socketcan', can_filters=None)    # allow all CAN-IDs
+    bus = can.Bus(channel='can0', interface='socketcan', can_filters=None)    # allow all CAN-IDs
 except OSError:
-	print('Cannot find CAN board.')
-	exit()
-	
+    print('Cannot find CAN board.')
+    exit()
+
 print('Ready')
 
 try:
-	while True:
-		message = bus.recv()	# Blocking wait until a message is received.
-		
-		c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
-		s=' ... '
-		for i in range(message.dlc ):
-			s +=  '{0:x} '.format(message.data[i])
-			print(' {}'.format(c+s))
-		
-		# pause 100mS, then loop for next message or until Ctrl-C
-		time.sleep(0.100) 
-		
+    while True:
+        message = bus.recv()    # Blocking wait until a message is received.
+        
+        c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
+        s=' ... '
+        for i in range(message.dlc ):
+            s +=  '{0:x} '.format(message.data[i])
+            print(' {}'.format(c+s))
+        
+        # pause 100mS, then loop for next message or until Ctrl-C
+        time.sleep(0.100) 
+        
 except KeyboardInterrupt:
-	# Catch keyboard interrupt
-	print('\n\rKeyboard interrupt')	
+    # Catch keyboard interrupt
+    print('\n\rKeyboard interrupt') 
 
 
-print('\n\rShutting bus and CAN0 down')	
+print('\n\rShutting bus and CAN0 down') 
 bus.shutdown()
 os.system("sudo /sbin/ip link set can0 down")
